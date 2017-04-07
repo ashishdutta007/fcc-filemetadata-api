e77e2e963932abd0875e26f94a7586dc2ff799de
ashishdutta007 <ashish.dutta050@gmail.com>
2017-04-07 17:53:54 +0530

    server code

--- server/server.js
+++ server/server.js
@@ -2,6 +2,18 @@ var express = require('express');
 var multer = require('multer');
 var app = express();
 
+app.use('/', function(request, response, next) {
+    console.log('Request for file metadata microservice');
+    request.on('error', function(error) {
+        return console.log('Error occurred', error);
+    });
+    response.on('error', function(error) {
+        return console.log('Error occurred', error);
+    });
+});
 
+app.post('/upload', function(request, response) {
+	
+});
 
 module.exports.app = app;
